2c718a0d335528069478681213f19e767b9fb3a2
Leslie Yip <leslieyip02@gmail.com>
2022-12-26 10:52:59 +0800

    Changed commas to semicolons

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -3,50 +3,50 @@ interface IControls {
 }
 
 interface ICurveData {
-    points: Array<THREE.Vector3>,
-    closed: boolean
-    extrudeShapeIndex: number,
-    moving?: boolean,
-    direction?: THREE.Vector3,
-    period?: number,
-    phase?: number
+    points: Array<THREE.Vector3>;
+    extrudeShapeIndex: number
+    closed?: boolean;
+    moving?: boolean;
+    direction?: THREE.Vector3;
+    period?: number;
+    phase?: number;
 }
 
 interface IPlatform {
-    mesh: THREE.Mesh,
-    origin: THREE.Vector3,
-    direction: THREE.Vector3,
-    period: number,
-    phase: number
+    mesh: THREE.Mesh;
+    origin: THREE.Vector3;
+    direction: THREE.Vector3;
+    period: number;
+    phase: number;
 }
 
 interface ITrackData {
-    startPoint: THREE.Vector3,
-    startDirection: THREE.Vector3,
-    startRotation: THREE.Euler,
-    curves: Array<ICurveData>,
-    extrudeShapes: Array<THREE.Shape>,
-    surfaceExtrudeShapes: Array<THREE.Shape>,
-    outlineExtrudeShapes: Array<THREE.Shape>,
-    extrudeOptions: THREE.ExtrudeGeometryOptions,
-    surfaceMaterial: THREE.Material,
-    outlineMaterial: THREE.Material,
-    backgroundColors: Array<string>,
-    gridColor: number | THREE.Color
+    startPoint: THREE.Vector3;
+    startDirection: THREE.Vector3;
+    startRotation: THREE.Euler;
+    curves: Array<ICurveData>;
+    extrudeShapes: Array<THREE.Shape>;
+    surfaceExtrudeShapes: Array<THREE.Shape>;
+    outlineExtrudeShapes: Array<THREE.Shape>;
+    extrudeOptions: THREE.ExtrudeGeometryOptions;
+    surfaceMaterial: THREE.Material;
+    outlineMaterial: THREE.Material;
+    backgroundColors: Array<string>;
+    gridColor: number | THREE.Color;
 }
 
 interface IVehicleData {
-    modelPath: string,
+    modelPath: string;
 
-    acceleration: number,
-    deceleration: number,
-    friction: number,
-    turnRate: number,
-    maxRoll: number,
+    acceleration: number;
+    deceleration: number;
+    friction: number;
+    turnRate: number;
+    maxRoll: number;
     
-    width: number,
-    height: number,
-    length: number
+    width: number;
+    height: number;
+    length: number;
 }
 
 export {
