002e225625aded17354da90ad36e3f4d9aef96bd
Leslie Yip <leslieyip02@gmail.com>
2023-01-01 20:15:24 +0800

    Added transition for resetting to last checkpoint

--- src/objects/Vehicle.ts
+++ src/objects/Vehicle.ts
@@ -30,6 +30,9 @@ export default class Vehicle {
     model: THREE.Group;
     hitbox: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
 
+    alive: boolean;
+    canMove: boolean;
+
     directionDebug?: DebugVector;
     normalDebug?: DebugVector;
 
@@ -56,6 +59,9 @@ export default class Vehicle {
         this.length = vehicleData.length;
 
         this.render(scene, vehicleData.modelPath, debug);
+
+        this.alive = true;
+        this.canMove = true;
     }
 
     loadGLTF(scene: THREE.Scene, data: GLTF) {
@@ -191,6 +197,9 @@ export default class Vehicle {
     }
 
     handleVehicleMovement(keysPressed: IControls, dt: number) {
+        if (!this.canMove)
+            return;
+
         // acceleration
         if (keysPressed["w"])
             this.velocity.add(this.direction.clone()
@@ -258,52 +267,86 @@ export default class Vehicle {
             this.directionDebug.update(this.direction.clone(), this.position.clone());
     }
 
-    handleCameraMovement(forward: boolean) {
-        let cameraPosition = this.position.clone();
+    handleCameraMovement(forward: boolean, follow: boolean = true) {
         let lookAtPosition = this.position.clone();
-        let offset = this.direction.clone().multiplyScalar(3);
 
-        if (forward) {
-            // set camera behind and above vehicle
-            cameraPosition.sub(offset);
-            lookAtPosition.add(this.direction);
-        } else {
-            cameraPosition.add(offset);
-            lookAtPosition.sub(this.direction);
+        // the camera will only follow the vehicle if it is in bounds
+        if (follow) {
+            let cameraPosition = this.position.clone();
+            let offset = this.direction.clone().multiplyScalar(3);
+    
+            if (forward) {
+                // set camera behind and above vehicle
+                cameraPosition.sub(offset);
+                lookAtPosition.add(this.direction);
+            } else {
+                cameraPosition.add(offset);
+                lookAtPosition.sub(this.direction);
+            }
+    
+            cameraPosition.y = cameraPosition.y + 1.5;
+            this.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
         }
 
-        cameraPosition.y = cameraPosition.y + 1.5;
-        this.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
         this.camera.lookAt(lookAtPosition);
+    }
+
+    resetToCheckpoint(checkpoint: ICheckpoint) {
+        // default checkpoint values
+        if (!checkpoint) {
+            this.position = new THREE.Vector3(0, 0, 0);
+            this.direction = new THREE.Vector3(1, 0, 0);
+            this.rotation = new THREE.Euler(0, Math.PI / 2, 0, "YZX");
+        } else {
+            this.position = checkpoint.mesh.position.clone();
+            this.direction = checkpoint.resetDirection.clone();
+            this.rotation = checkpoint.resetRotation.clone();
+        }
+        
+        this.velocity = new THREE.Vector3(0, 0, 0);
+        this.model.position.set(this.position.x, this.position.y, this.position.z);
+        this.hitbox.position.set(this.position.x, this.position.y, this.position.z);
+        this.model.setRotationFromEuler(this.rotation.clone());
+        this.hitbox.setRotationFromEuler(this.rotation.clone());
     }
 
     update(keysPressed: IControls, track?: Track, dt?: number) {
         if (!this.model || !this.hitbox || !track || !dt)
             return;
-
+    
         this.gravity = defaultGravity;
-
-        this.handleTrackCollision(track);
+        this.handleTrackCollision(track);        
         this.handleVehicleMovement(keysPressed, dt);
 
-        // reset vehicle to last checkpoint if it falls out of bounds
-        if (this.position.y < -30) {
-            if (!this.checkpoint) {
-                this.position = new THREE.Vector3(0, 0, 0);
-            } else {
-                this.position = this.checkpoint.mesh.position.clone();
-                this.direction = this.checkpoint.resetDirection.clone();
-                this.rotation = this.checkpoint.resetRotation.clone();
+        // reset vehicle to last checkpoint if it falls out of bounds        
+        if (this.position.y < -30 || !this.alive) {
+            let curtain = document.getElementById("curtain");
+            curtain.classList.add("fade-to-black");
 
-                this.velocity = new THREE.Vector3(0, 0, 0);
-                this.model.setRotationFromEuler(this.rotation.clone());
-                this.hitbox.setRotationFromEuler(this.rotation.clone());
-            }
+            if (this.alive) {
+                this.alive = false;
+
+                setTimeout(() => {
+                    this.resetToCheckpoint(this.checkpoint);
+                    this.alive = true;
+                    this.canMove = false;
+
+                    curtain.classList.remove("fade-to-black");
+                    curtain.style.opacity = "100";
+                    curtain.classList.add("scroll-up");
+                    
+                    setTimeout(() => {
+                        curtain.classList.remove("scroll-up");
+                        curtain.style.opacity = "0";
+                        this.canMove = true;
+                    }, 1000)
+                }, 900);
+            } 
         }
-
+        
         if (!this.manualCamera) {
             let forward = !keysPressed["r"];
-            this.handleCameraMovement(forward);
+            this.handleCameraMovement(forward, this.alive);
         }
     }
 }
