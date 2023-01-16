3a3fb59a7988ac5673877cc7cff96fd8b8082242
Leslie Yip <leslieyip02@gmail.com>
2023-01-16 22:56:24 +0800

    Added a checkerboard texture

--- src/objects/StartLine.ts
+++ src/objects/StartLine.ts
@@ -5,13 +5,26 @@ export default class StartLine extends THREE.Group {
         scene: THREE.Scene) {
             
         super();
-        let bannerMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc, side: THREE.DoubleSide });
-        let bannerGeometry = new THREE.PlaneGeometry(36, 5);
-        let bannerMesh = new THREE.Mesh(bannerGeometry, bannerMaterial);
-        bannerMesh.position.set(position.x, position.y + 8, position.z);
-        bannerMesh.setRotationFromEuler(rotation);
-        this.add(bannerMesh);
-        
+        this.loadPoles(position);
+        this.loadBanner(position, rotation)
+            .then(() => scene.add(this));
+    }
+
+    async loadBanner(position: THREE.Vector3, rotation: THREE.Euler) {
+        let loader = new THREE.TextureLoader();
+        await loader.loadAsync("../../assets/textures/checkerboard.jpg")
+            .then(texture => {
+                console.log(texture)
+                let bannerMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+                let bannerGeometry = new THREE.PlaneGeometry(36, 5);
+                let bannerMesh = new THREE.Mesh(bannerGeometry, bannerMaterial);
+                bannerMesh.position.set(position.x, position.y + 8, position.z);
+                bannerMesh.setRotationFromEuler(rotation);
+                this.add(bannerMesh);
+            });
+    }
+
+    loadPoles(position: THREE.Vector3) {
         let polePoints = [
             new THREE.Vector3(position.x, -2, position.z + 18),
             new THREE.Vector3(position.x, position.y + 12, position.z + 18)
@@ -26,7 +39,5 @@ export default class StartLine extends THREE.Group {
         poleMesh = poleMesh.clone();
         poleMesh.translateZ(-36);
         this.add(poleMesh);
-
-        scene.add(this);
     }
 }
